c532ead4cf8c1e8bed8701c2621cf77db707ce7c
zcl <qlzcl@xx.com>
2020-09-13 20:33:49 +0800

    添加项目文件。

---
+++ BlazorHosted/Server/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorHosted.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorHosted.Server.Controllers
+{
+   
+    public class TestController : ControllerBase
+    {
+        [Route("api/test1")]
+        public string Test1()
+        {
+            return "response 1";
+        }
+
+        [Route("api/test2")]
+        public TestObject test2()
+        {
+            return new TestObject(){a=5,b="test string",c=0.5};
+        }
+
+        [Route("api/test3")]
+        public TestObject test3([FromBody] TestObject tobj)
+        {
+            tobj.a += 15;
+            tobj.b += " work and uses header: " + Request.Headers["headervalue"];
+            tobj.c = 15;
+            return tobj;
+        }
+
+        [Route("api/test4")]
+        public TestObject Test4([FromForm] TestObject tobj)
+        {
+            tobj.a += 15;
+            tobj.b += " works and uses header: " + Request.Headers["headervalue"];
+            tobj.c = 15;
+            return tobj;
+        }
+    }
+}
---
+++ BlazorHosted/Shared/TestObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorHosted.Shared
+{
+    public class TestObject
+    {
+        public int a { get; set; }
+        public string b { get; set; }
+        public double c { get; set; }
+
+    }
+}
